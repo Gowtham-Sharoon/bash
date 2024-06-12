237391ccf23e7151be48ec391c829878df56626b
Gowtham-Sharoon <gowthamsharoon@outlook.com>
2024-06-12 12:22:58 +0530

    complete backup script

--- backup.sh
+++ backup.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+
+SOURCE="/home/sharun/Documents/DevOps"
+DESTINATION="/home/sharun/Documents/backup"
+DATE=$(date +%Y-%m-%d_%H-%M-%S)
+
+# Create backup directory 
+mkdir -p $DESTINATION
+
+
+# Create a compressed backup file
+tar -czf $DESTINATION/backup_$DATE.tar.gz -C $SOURCE .
+
+echo "Backup completed on $DATE"
 
